ebcd87d6a3706d57257370cc8a6c487d5d1c2dc5
yash2k4 <yash2k4@users.noreply.github.com>
2026-02-01 16:54:24 +0530

    Add nvim to sys pkg

--- configuration.nix
+++ configuration.nix
@@ -20,63 +20,24 @@
   services.displayManager.ly.enable = true;
   services.displayManager.ly.settings.session_log = false;
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
+  programs.hyprland.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-    XDG_SESSION_TYPE = "wayland";
-  };
+  hardware.graphics.enable = true;
 
   programs.zsh.enable = true;
-
-  i18n.defaultLocale = "en_US.UTF-8";
 
   users.users.yashy = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
 
   nixpkgs.config.allowUnfree = true;
 
-  fonts.fontconfig.enable = true;
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    noto-fonts-cjk-sans
-    noto-fonts-color-emoji
-    bibata-cursors
-  ];
-
-  services.upower.enable = true;
-  services.tlp.enable = true;
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  virtualisation.docker.enable = true;
-
-  security.polkit.enable = true;
-  services.dbus.enable = true;
-
   environment.systemPackages = with pkgs; [
+    neovim
     docker
     ly
     hypridle
@@ -91,8 +52,6 @@
     "nix-command"
     "flakes"
   ];
-
-  services.openssh.enable = true;
 
   system.stateVersion = "25.11";
 }
