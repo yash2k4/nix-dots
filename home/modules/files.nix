15e83bbfb4d9241b4c351d9e9d4240d26ba7ba45
yash2k4 <yash2k4@users.noreply.github.com>
2026-02-01 15:54:00 +0530

    fix home file source paths

--- home/modules/files.nix
+++ home/modules/files.nix
@@ -2,13 +2,13 @@
 
 {
   home.file = {
-    ".config/hypr".source        = ../config/hypr;
-    ".config/kitty".source       = ../config/kitty;
-    ".config/nvim".source        = ../config/nvim;
-    ".config/fuzzel".source      = ../config/fuzzel;
-    ".config/yazi".source        = ../config/yazi;
-    ".config/lazydocker".source  = ../config/lazydocker;
-    ".config/lazygit".source     = ../config/lazygit;
-    ".config/fastfetch".source   = ../config/fastfetch;
+    ".config/hypr".source        = ../../config/hypr;
+    ".config/kitty".source       = ../../config/kitty;
+    ".config/nvim".source        = ../../config/nvim;
+    ".config/fuzzel".source      = ../../config/fuzzel;
+    ".config/yazi".source        = ../../config/yazi;
+    ".config/lazydocker".source  = ../../config/lazydocker;
+    ".config/lazygit".source     = ../../config/lazygit;
+    ".config/fastfetch".source   = ../../config/fastfetch;
   };
 }
