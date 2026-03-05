4c213419291cf0eddbd772383c9dbc9290c4597d
yash2k4 <yashupress@gmail.com>
2026-03-06 01:11:22 +0530

    add bash alias

--- home.nix
+++ home.nix
@@ -38,6 +38,21 @@
       };
     };
 
+    bash = {
+      enable = true;
+      shellAliases = {
+        nixro = "sudo nixos-rebuild switch --flake ~/nix-dots#nixro --accept-flake-config";
+        nixrt = "sudo nixos-rebuild test --flake ~/nix-dots#nixro --accept-flake-config";
+        nixrb = "sudo nixos-rebuild build --flake ~/nix-dots#nixro --accept-flake-config";
+        nixup = "cd ~/nix-dots && nix flake update && sudo nixos-rebuild switch --flake .#nixro --accept-flake-config";
+        nixgc = "sudo nix-collect-garbage -d";
+        vim = "nvim";
+        ls = "eza --icons --group-directories-first";
+        ll = "eza -la --icons --group-directories-first --git";
+        tree = "eza --tree --icons";
+      };
+    };
+
     zsh = {
       enable = true;
       initContent = ''
