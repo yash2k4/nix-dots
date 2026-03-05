e533ccf66269083064f92616870267fe0f5954fa
yash2k4 <yashupress@gmail.com>
2026-03-06 00:53:06 +0530

    add other files

--- home.nix
+++ home.nix
@@ -1,15 +1,9 @@
 { config, pkgs, inputs, ... }:
-
 {
-  imports = [
-    inputs.noctalia.homeModules.default
-  ];
-
   home = {
     username = "yash2k4";
     homeDirectory = "/home/yash2k4";
     stateVersion = "25.11";
-
     packages = [
       (pkgs.writeShellApplication {
         name = "ns";
@@ -25,13 +19,11 @@
   programs = {
     git = {
       enable = true;
-
       settings = {
         user = {
           name = "yash2k4";
           email = "yash2k4@users.noreply.github.com";
         };
-
         init.defaultBranch = "main";
         core.editor = "nvim";
         pull.rebase = true;
@@ -48,7 +40,6 @@
 
     zsh = {
       enable = true;
-
       initContent = ''
 if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
 source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
@@ -139,18 +130,34 @@ zinit ice wait"0" lucid
 zinit light zsh-users/zsh-syntax-highlighting
 '';
     };
-
-    noctalia-shell = {
-      enable = true;
-      systemd.enable = true;
-    };
   };
 
-  home.file.".config/niri".source = ./config/niri;
   home.file.".config/alacritty".source = ./config/alacritty;
+  home.file.".config/btop".source = ./config/btop;
+  home.file.".config/cava".source = ./config/cava;
+  home.file.".config/doom".source = ./config/doom;
+  home.file.".config/dunst".source = ./config/dunst;
+  home.file.".config/dwm".source = ./config/dwm;
+  home.file.".config/fastfetch".source = ./config/fastfetch;
+  home.file.".config/flameshot".source = ./config/flameshot;
+  home.file.".config/foot".source = ./config/foot;
+  home.file.".config/fuzzel".source = ./config/fuzzel;
+  home.file.".config/hypr".source = ./config/hypr;
+  home.file.".config/i3".source = ./config/i3;
+  home.file.".config/kitty".source = ./config/kitty;
+  home.file.".config/lazydocker".source = ./config/lazydocker;
+  home.file.".config/lazygit".source = ./config/lazygit;
+  home.file.".config/nvim".source = ./config/nvim;
+  home.file.".config/picom".source = ./config/picom;
+  home.file.".config/polybar".source = ./config/polybar;
+  home.file.".config/rofi".source = ./config/rofi;
+  home.file.".config/scripts".source = ./config/scripts;
+  home.file.".config/slstatus".source = ./config/slstatus;
+  home.file.".config/st".source = ./config/st;
+  home.file.".config/superfile".source = ./config/superfile;
+  home.file.".config/sway".source = ./config/sway;
+  home.file.".config/swaylock".source = ./config/swaylock;
+  home.file.".config/waybar".source = ./config/waybar;
   home.file.".config/yazi".source = ./config/yazi;
   home.file.".config/zathura".source = ./config/zathura;
-  home.file.".config/ghostty".source = ./config/ghostty;
-  home.file.".config/fuzzel".source = ./config/fuzzel;
-  home.file.".config/nvim".source = ./config/nvim;
 }
