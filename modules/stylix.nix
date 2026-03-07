7169d6c3282a97b85796afb4e15701a311faf468
yash2k4 <yash2k4@users.noreply.github.com>
2026-03-07 16:44:47 +0530

    increase font size

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -27,10 +27,10 @@
       };
 
       sizes = {
-        applications = 12;
-        terminal = 14;
-        desktop = 11;
-        popups = 11;
+        applications = 13;
+        terminal = 15;
+        desktop = 12;
+        popups = 12;
       };
     };
 
