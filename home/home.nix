db0197363abccfb7e7f17b3b62084b765dca8e05
yash2k4 <yash2k4@users.noreply.github.com>
2026-03-13 23:14:13 +0530

    chore: rename apps to dents(dendrites)

--- home/home.nix
+++ home/home.nix
@@ -2,13 +2,13 @@
 {
   imports = [
     ./dotfiles.nix
-    ./apps/bash.nix
-    ./apps/git.nix
-    ./apps/nix-search.nix
-    ./apps/noctalia.nix
-    ./apps/spicetify.nix
-    ./apps/vesktop.nix
-    ./apps/zsh.nix
+    ./dents/bash.nix
+    ./dents/git.nix
+    ./dents/nix-search.nix
+    ./dents/noctalia.nix
+    ./dents/spicetify.nix
+    ./dents/vesktop.nix
+    ./dents/zsh.nix
   ];
   home = {
     username = "yash2k4";
