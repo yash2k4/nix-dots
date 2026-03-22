4ab1746c316ed756480abbd3dce5ee4aadf95409
yash2k4 <yash2k4@users.noreply.github.com>
2026-03-23 03:38:05 +0530

    chore: add few services

--- hosts/nixro/configuration.nix
+++ hosts/nixro/configuration.nix
@@ -1,8 +1,4 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/nvidia.nix
@@ -26,20 +22,7 @@
 
   hardware.bluetooth.enable = true;
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
+  i18n.defaultLocale = "en_US.UTF-8";
 
   networking = {
     hostName = "nixro";
--- modules/services.nix
+++ modules/services.nix
@@ -1,14 +1,15 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   networking = {
     firewall = {
       enable = true;
+
       allowedTCPPorts = [
         22
         80
         443
         53317
       ];
+
       allowedUDPPorts = [
         53317
         5353
@@ -16,7 +17,24 @@
     };
   };
 
+  security.polkit.enable = true;
+
   services = {
+    acpid.enable = true;
+
+    avahi.enable = true;
+
+    blueman.enable = true;
+
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+    };
+
+    earlyoom.enable = true;
+
+    fwupd.enable = true;
+
     greetd = {
       enable = true;
       settings = {
@@ -27,27 +45,36 @@
       };
     };
 
+    gvfs.enable = true;
+
     openssh.enable = true;
 
-    power-profiles-daemon.enable = false;
+    printing.enable = true;
+
+    resolved.enable = true;
 
     tlp = {
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
-        DISK_DEVICES = "nvme0n1 sda";
-        DISK_APM_LEVEL_ON_AC = "254 254";
-        DISK_APM_LEVEL_ON_BAT = "128 128";
+
+        DISK_DEVICES = "nvme0n1";
+
         RUNTIME_PM_ON_AC = "auto";
         RUNTIME_PM_ON_BAT = "auto";
+
         PCIE_ASPM_ON_AC = "default";
         PCIE_ASPM_ON_BAT = "powersupersave";
+
         USB_AUTOSUSPEND = 1;
+
         WIFI_PWR_ON_AC = "off";
         WIFI_PWR_ON_BAT = "on";
       };
@@ -55,14 +82,22 @@
 
     thermald.enable = true;
 
+    tumbler.enable = true;
+
     upower.enable = true;
   };
 
+  programs = {
+    gnupg.agent.enable = true;
+    virt-manager.enable = true;
+  };
+
   virtualisation = {
-    docker.enable = true;
+    docker = {
+      enable = true;
+      autoPrune.enable = true;
+    };
 
     libvirtd.enable = true;
   };
-
-  programs.virt-manager.enable = true;
 }
