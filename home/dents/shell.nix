0fc1488a1a8c6893f1c6fcac2a5302927ad9b278
yash2k4 <yash2k4@users.noreply.github.com>
2026-03-29 17:20:59 +0530

    chore(zsh): tinker

--- home/dents/shell.nix
+++ home/dents/shell.nix
@@ -57,7 +57,6 @@ in {
       SAVEHIST=100000
       setopt INC_APPEND_HISTORY
       setopt SHARE_HISTORY
-      setopt HIST_IGNORE_DUPS
       setopt HIST_IGNORE_SPACE
       setopt HIST_REDUCE_BLANKS
       setopt EXTENDED_HISTORY
