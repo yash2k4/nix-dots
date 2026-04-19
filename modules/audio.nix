38f7bbdb99143440079b2c1980b9c564d4e6daf2
yash2k4 <yash2k4@users.noreply.github.com>
2026-04-20 00:05:25 +0530

    chore(audio): better audio support

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,14 +1,17 @@
 { ... }:
-
 {
   services = {
     pipewire = {
       enable = true;
       alsa.enable = true;
+      alsa.support32Bit = true;
       pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
     };
 
     pulseaudio.enable = false;
+
   };
 
   security = {
