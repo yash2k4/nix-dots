2c86bd72ea322ac0e323ded53f00a632d100245d
yash2k4 <yash2k4@users.noreply.github.com>
2026-04-21 16:50:45 +0530

    chore(zsh): add go-bin path

--- home/programs/sh.nix
+++ home/programs/sh.nix
@@ -12,6 +12,9 @@ in {
     initExtra = ''
       eval "$(fzf --bash)"
       eval "$(zoxide init bash)"
+
+      export GOPATH="$HOME/.local/share/go"
+      export PATH="$PATH:$GOPATH/bin"
       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
     '';
   };
@@ -29,10 +32,9 @@ in {
         mkdir -p "$(dirname $ZINIT_HOME)"
         git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
       fi
+
       source "''${ZINIT_HOME}/zinit.zsh"
-
       zinit ice depth=1; zinit light romkatv/powerlevel10k
-
       zinit ice wait"0" lucid; zinit light zsh-users/zsh-syntax-highlighting
       zinit ice wait"0" lucid; zinit light Aloxaf/fzf-tab
 
@@ -49,7 +51,7 @@ in {
       bindkey '^[[1;5C' forward-word
 
       HISTFILE="''${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
-      mkdir -p "''${HISTFILE:h}"
+      mkdir -p "$(dirname "$HISTFILE")"
       HISTSIZE=100000
       SAVEHIST=100000
       setopt SHARE_HISTORY HIST_IGNORE_SPACE HIST_REDUCE_BLANKS EXTENDED_HISTORY HIST_SAVE_NO_DUPS HIST_IGNORE_ALL_DUPS
@@ -66,9 +68,11 @@ in {
       path=(
         "$HOME/.cargo/bin"
         "$HOME/.config/scripts"
+        "$HOME/.local/share/go/bin"
         $path
       )
 
+      export GOPATH="$HOME/.local/share/go"
       export EDITOR=nvim
       export VISUAL="$EDITOR"
       export SUDO_EDITOR="$EDITOR"
