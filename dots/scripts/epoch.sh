d07ce32ec6189c3380a924998d3ba324b9eb09c6
yash2k4 <yash2k4@users.noreply.github.com>
2026-04-27 22:37:29 +0530

    chore(scripts): better output

--- dots/scripts/epoch.sh
+++ dots/scripts/epoch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+MAUVE='\033[38;2;203;166;247m'
+RESET='\033[0m'
+
 install_time=$(date -d "2025-09-20 23:47:23" +%s)
 current_time=$(date +%s)
 install_datetime=$(date -d "@$install_time" "+%d-%m-%Y %H:%M:%S")
@@ -20,19 +23,22 @@ years=$((total_months / 12))
 
 p() {
     if [[ $1 -eq 1 ]]; then
-        printf "%s %s" "$1" "$2"
+        printf "${MAUVE}%s${RESET} %s" "$1" "$2"
     else
-        printf "%s %ss" "$1" "$2"
+        printf "${MAUVE}%s${RESET} %ss" "$1" "$2"
     fi
 }
 
-echo "initial linux install time:$install_datetime"
-echo "current time:$current_datetime"
+echo -e "initial linux install time: ${MAUVE}${install_datetime}${RESET}"
+echo -e "current time: ${MAUVE}${current_datetime}${RESET}"
+
 echo
 echo "time elapsed since then:"
-printf "in seconds:%s\n" "$(p "$age_seconds" second)"
-printf "in minutes:%s, %s\n" "$(p "$total_minutes" minute)" "$(p "$seconds" second)"
-printf "in hours:%s, %s, %s\n" "$(p "$total_hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
-printf "in days:%s, %s, %s, %s\n" "$(p "$total_days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
-printf "in months:%s, %s, %s, %s, %s\n" "$(p "$total_months" month)" "$(p "$days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
-printf "in years:%s, %s, %s, %s, %s, %s\n" "$(p "$years" year)" "$(p "$months" month)" "$(p "$days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
+echo
+
+printf "in seconds: %s\n" "$(p "$age_seconds" second)"
+printf "in minutes: %s, %s\n" "$(p "$total_minutes" minute)" "$(p "$seconds" second)"
+printf "in hours: %s, %s, %s\n" "$(p "$total_hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
+printf "in days: %s, %s, %s, %s\n" "$(p "$total_days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
+printf "in months: %s, %s, %s, %s, %s\n" "$(p "$total_months" month)" "$(p "$days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
+printf "in years: %s, %s, %s, %s, %s, %s\n" "$(p "$years" year)" "$(p "$months" month)" "$(p "$days" day)" "$(p "$hours" hour)" "$(p "$minutes" minute)" "$(p "$seconds" second)"
